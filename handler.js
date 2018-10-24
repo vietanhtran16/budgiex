e66b14a198eb382ff3cd4fe144d39510f5b337f2
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-25 11:48:24 +1300

    serverless

--- handler.js
+++ handler.js
@@ -8,4 +8,7 @@ module.exports.hello = async (event, context) => {
       input: event,
     }),
   };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
