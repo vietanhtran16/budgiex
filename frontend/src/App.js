dbc0d8b7c5df462c37a10bd1d46d40672ed9a68a
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-25 15:04:49 +1300

    front page basic af

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Camera from 'react-camera';
 import {Input, Button, Card} from '@myob/myob-widgets';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div>
-          <Button type="primary">Primary button</Button>
-        </div>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+                <div>
+                    <p>Welcome to</p>
+                    <img src="/images/budgiex-logo.png" alt="logo"/>
+                    <p>Budgeting made simple.</p>
+                    <Input name="income" label="Amount to budget monthly (income)" placeholder="$0.00"/>
+                    <Button type="primary">Next</Button>
+                </div>
+        );
+    }
 }
 
 export default App;
