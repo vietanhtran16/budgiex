0347ab5779eac3478e27ba7e669766c18dde74f6
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-25 17:05:15 +1300

    set up page

--- frontend/src/SetUpPage.js
+++ frontend/src/SetUpPage.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
-import {Table, Icons} from '@myob/myob-widgets';
+import {Table, Icons, Button} from '@myob/myob-widgets';
 import './App.css';
 
 class SetUpPage extends Component {
+    navigateToSetUpPage(){
+
+    }
+
     render() {
         let masterCategories = ['Everyday Expenses', 'Living Expenses', 'Regular Expenses', 'Irregular Expenses', 'Savings', 'Personal', 'Debt'];
         let newVar =
@@ -17,9 +21,18 @@ class SetUpPage extends Component {
             </Table.CollapsibleRow>;
         });
         return (
+            <div>
+                <div id="back-button"><Icons.LeftChevron size="2em"/>Back</div>
             <Table.Body>
+                <Table.Header>
+                    <Table.HeaderItem>MASTER CATEGORIES</Table.HeaderItem>
+                    <Table.HeaderItem>BUDGETED</Table.HeaderItem>
+                </Table.Header>
                 {newVar}
+                {<Table.Row><Table.RowItem><Icons.Add size="1em"/> Add Category</Table.RowItem></Table.Row>}
             </Table.Body>
+            <Button id="save-button" type="primary">Save</Button>
+            </div>
         );
     }
 }
