305e317eaa4c158b2dd888c6d3b9ea36235547d4
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-25 14:43:12 +1300

    feelix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,16 @@
 import React, { Component } from 'react';
 import Camera from 'react-camera';
+import {Input, Button, Card} from '@myob/myob-widgets';
 import './App.css';
 
-const style = {
-  preview: {
-    position: 'relative',
-    maxWidth: '50%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  }
-  
-};
-
 class App extends Component {
-  takePicture() {
-    this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      console.log(this.img);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-    })
-  }
-
   render() {
     return (
-      <div style={style.container}>
-        <Camera
-          style={style.preview}
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-        >
-          <div style={style.captureContainer} onClick={() => this.takePicture()}>
-            <div style={style.captureButton} />
-          </div>
-        </Camera>
-        <div style={style.preview}>
-          <img
-            ref={(img) => {
-              this.img = img;
-            }}
-          />
+      <React.Fragment>
+        <div>
+          <Button type="primary">Primary button</Button>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
