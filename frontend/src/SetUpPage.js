495984fc230b804d07e6d72a9b39bc60bc167d1a
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-25 15:35:24 +1300

    starbird components

---
+++ frontend/src/SetUpPage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Icons, Dropdown } from "@starbird/myob-widgets";
+import './App.css';
+
+class Page extends Component {
+
+    onSelected(selected) { this.setState({ selected }); }
+    render() {
+        return (
+            <div>
+                <Dropdown
+                    items={items}
+                    onSelect={this.onSelected}
+                    toggle={<Dropdown.Toggle>Everyday Expenses <Icons.Caret /></Dropdown.Toggle>}
+                />
+            </div>
+        );
+    }
+}
+
+export default SetUpPage;
--- frontend/src/SplashPage.js
+++ frontend/src/SplashPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Input, Button} from '@myob/myob-widgets';
+import {Input, Button} from '@starbird/myob-widgets';
 import './App.css';
 
 class SplashPage extends Component {
@@ -7,7 +7,7 @@ class SplashPage extends Component {
         return (
             <div>
                 <p>Welcome to</p>
-                <img src="images/budgiex-logo.png" alt="logo"/>
+                <img src={require('./images/budgiex-logo.png')} alt={"logo"}/>
                 <p>Budgeting made simple.</p>
                 <Input name="income" label="Amount to budget monthly (income)" placeholder="$0.00"/>
                 <Button type="primary">Next</Button>
