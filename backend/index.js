7acd87c43e25eeb7e6fae1c5b215344d25c36071
max tang <maxtang@max-MacBook-Pro.myob.co.nz>
2018-10-26 13:40:43 +1300

    Receipt processing

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,39 @@ app.get("/analyseImage", (req, res) => {
     .documentTextDetection(`https://s3-ap-southeast-2.amazonaws.com/budgiex/${req.query["fileName"]}`)
     .then(results => {
       const detections = results[0].fullTextAnnotation;
-      res.send(detections);
+      let text = detections.text;
+      let list = text.split('\n');
+      var total = 0;
+      var seller = list[0];
+
+      var responseDict = [];
+      responseDict['seller'] = seller;
+
+      var itemDict = [];
+      for (var i = 0; i < list.length; i++) {
+          var item = list[i];
+          var indexOfMoney = item.search(/\$\d+(\.\d+)$/);
+
+          if (indexOfMoney == -1) continue;
+          var indexOfDiscount = item.search("% off");
+          if (indexOfDiscount > -1) continue;
+
+          if (item.startsWith("TOTAL")) {
+              total = item.substring(indexOfMoney + 1);
+              // console.log("TOTAL: " + total);
+              break;
+          }
+
+          if (item.search("SUBTOTAL") > -1) continue;
+          if (item.search("GST.") > -1) continue;
+          
+          itemDict.push({'name': item.substring(0, indexOfMoney), 'cost': item.substring(indexOfMoney + 1)});
+      }
+      responseDict["items"] = itemDict;
+      res.json(responseDict);
     })
     .catch(err => {
-      res.send(`Error: ${err}`);
+      res.josn({"error": err});
     });
 });
 
